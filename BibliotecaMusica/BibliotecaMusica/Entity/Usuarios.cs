15690ef61b9e5c37e8a10c096efa984ee68b571b
brahmaxd <abraham.antil.ra@gmail.com>
2013-10-25 02:09:07 -0300

    Entity Usuario
    hola

---
+++ BibliotecaMusica/BibliotecaMusica/Entity/Usuarios.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BibliotecaMusica.Entity
+{
+    class Usuarios
+    {
+        private String usuario;
+
+        public String Usuario
+        {
+            get { return usuario; }
+            set { usuario = value; }
+        }
+        private String clave;
+
+        public String Clave
+        {
+            get { return clave; }
+            set { clave = value; }
+        }
+        private int esAdmin;
+
+        public int EsAdmin
+        {
+            get { return esAdmin; }
+            set { esAdmin = value; }
+        }
+
+        public Usuarios()
+        {
+            Usuario = "";
+
+        }
+        public Usuarios(String usuario, String clave, int esAdmin)
+        {
+            Usuario = usuario;
+            Clave = clave;
+            EsAdmin = esAdmin;
+        }
+    }
+}
